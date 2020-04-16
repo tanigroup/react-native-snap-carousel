fa806341a0abf1be7f99a8e431e702500462a845
irvanseptian0809 <irvanseptian80@gmail.com>
2020-04-16 16:48:42 +0700

    Update Carousel.js

--- src/carousel/Carousel.js
+++ src/carousel/Carousel.js
@@ -1131,7 +1131,7 @@ export default class Carousel extends Component {
     }
 
     snapToNext (animated = true, fireCallback = true) {
-        const itemsLength = this._getCustomDataLength();
+        const itemsLength = this.props.data.length;
 
         let newIndex = this._activeItem + 1;
         if (newIndex > itemsLength - 1) {
